a6c8d375c47ae8b3d2fed27a3e20e8aeb9d2511c
zxh2135645 <zxh2135645@gmail.com>
2017-03-13 14:24:26 -0700

    minor changes

--- doit_arg.py
+++ doit_arg.py
@@ -5,16 +5,21 @@ from utils import doit_workflow
 import sys
 from glob import glob
 import numpy as np
+import csv
 
 if __name__ == '__main__':
     print("Reading file from the path: ", sys.argv[1], "\n")
     data_ref = glob(sys.argv[1])
     print(data_ref)
 
+
     FLAIR_T1_name = data_ref[0].split('/')[5]
-    output_dir = '/data/henry1/tristan/LST/opt_thresh_results'
+    output_dir = '/data/henry1/tristan/LST/opt_thresh_results/test_subjects'
     sk_dir = os.path.join(output_dir, FLAIR_T1_name)
     thresh_array = np.linspace(0.05, 1.00, num=20)
     dwf = doit_workflow(data_ref, thresh_array, sink_dir=sk_dir)
 
     dwf.run()
+
+    with open(os.path.join(sk_dir, '_bin_thresh_'+thresh_array[i]), newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
